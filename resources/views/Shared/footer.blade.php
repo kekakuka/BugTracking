92b5c4385f70630dc0e0d07eb0820350fbcace22
Dreamufor <azimeng520@gmail.com>
2018-11-21 15:54:03 +1300

    footerStyle

--- resources/views/Shared/footer.blade.php
+++ resources/views/Shared/footer.blade.php
@@ -1,47 +1,33 @@
 
 
-    <footer class="footer" style="margin:50px 0px -20px 0px; padding:20px; background: linear-gradient(rgba(233, 235, 235, 0.71),rgba(158, 160, 160, 0.71));">
-        <div class="container">
-        <div class="row">
-            <div class="col-md-3 col-xs-6">
-
-                <div>
-                    <p><span style="font-size:20px" class="glyphicon glyphicon-user"> </span> Lecturer: Natalia</p>
-
-
-                </div>
-                <div>
-                    <p><span style="font-size:20px" class="glyphicon glyphicon-user"> </span> Developer/Tester:Lei Li
-                    </p>
-
+    <footer class="footer">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-4">
+                    <h5 class="text-uppercase">Bug Tracking System</h5>
+                    <p class="footerStyle">The website is a software application that keeps track of reported software bugs in software development projects.</p>
                 </div>
 
-
-            </div>
-            <div class="col-md-4 col-xs-6">
-
-
-                <div>
-                    <p><span style="font-size:20px" class="glyphicon glyphicon-user"> </span> UI Designer/Tester: MengXue Zhang</p>
+                <div class="col-md-4">
+                    <h5 class="text-uppercase">Contact us</h5>
+                    <div class="footerStyle">
+                        Unitec Institute of Technology <br>
+                        139 Carrington Rd  <br> Mount Albert <br> Auckland 1025
+                        </div>
                 </div>
-
-                <div>
-                    <p><span style="font-size:20px" class="glyphicon glyphicon-user"> </span> Designer/Tester: LiHui Jiang</p>
+                <div class="col-md-4">
+                    <h5 class="text-uppercase">
+                        ISCG7413 Testing & QA Management
+                    </h5>
+                    <p class="footerStyle">Lecturer: Natalia
+                        <br>Developer: Lei li
+                        <br>Designer: Mengxue Zhang
+                        <br>Tester: LiHui Jiang</p>
                 </div>
 
             </div>
-            <div class="col-md-5 col-xs-12">
-                <div>
-
-                    <p><span style="font-size:20px" class="glyphicon glyphicon  glyphicon-globe"> </span> Building
-                        183, 139 Carrington Road Mt Albert</p>
-                </div>
-                <div>
-                    <p>&copy; 2018 - Bug Tracking System</p>
-                </div>
-            </div>
-
-        </div>
+            <hr class="clearfix w-100 d-md-none" style="margin-top: 0 !important; margin-bottom: 3px; !important;">
+            <div class="footerStyle">© 2018 - Bug Tracking System</div>
         </div>
     </footer>
 
